8e10dd6bad589b6411b8573f7f1adb0753e5587c
BILAL74777 <billhssn777@icloud.com>
2025-07-29 15:24:28 +0500

    updated

--- Controllers/DatabaseController.php
+++ Controllers/DatabaseController.php
@@ -2,11 +2,11 @@
 function OpenCon()
 {
     $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
+    $dbuser = "u103076311_trans_kwm895";
+    $dbpass = "p0q~?FqM]";
     //$dbuser = "root";
     //$dbpass = "";
-    $db = "apnasole_mwk_transport";
+    $db = "u103076311_trans_kwm";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
     return $conn;
 }
