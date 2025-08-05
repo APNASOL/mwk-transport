a6ae0f358bda156674f1348cfd9a05989af437e4
BILAL74777 <billhssn777@icloud.com>
2025-08-05 16:50:33 +0500

    updated changes

--- Controllers/DatabaseController.php
+++ Controllers/DatabaseController.php
@@ -1,22 +1,22 @@
 <?php
-function OpenCon()
-{
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = ""; 
-    $db = "apnasole_mwk_transport";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
-    return $conn;
-}
 // function OpenCon()
 // {
 //     $dbhost = "localhost";
-//     $dbuser = "u103076311_trans_kwm895";
-//     $dbpass = "p0q~?FqM]"; 
-//     $db = "u103076311_trans_kwm";
+//     $dbuser = "root";
+//     $dbpass = ""; 
+//     $db = "apnasole_mwk_transport";
 //     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 //     return $conn;
 // }
+function OpenCon()
+{
+    $dbhost = "localhost";
+    $dbuser = "u103076311_trans_kwm895";
+    $dbpass = "p0q~?FqM]"; 
+    $db = "u103076311_trans_kwm";
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
+    return $conn;
+}
 
 function CloseCon($conn)
 {
